667a9aaccfef6c3ed31fed311f740e47a63eb1d8
Андрей Рябченко <ryabchenko.a@delcomp.ru>
2020-11-06 14:07:05 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function ($) {
+
+    // $("input[type='tel']").inputmask({"mask": "+7(999) 999-9999"});
+
+
+    // var certificatesSwiper = new Swiper('.certificates-slider__slider .swiper-container', {
+    //     loop: false,
+    //     slidesPerView: 5,
+    //     spaceBetween: 24,
+    //     speed: 600,
+    //      // autoHeight: true,
+    //     // autoplay: {
+    //     //     delay: 3000
+    //     // },
+    //     navigation: {
+    //         nextEl: '.certificates-slider__slider .certificates-slider__slider-next',
+    //         prevEl: '.certificates-slider__slider .certificates-slider__slider-prev',
+    //     },
+    //       pagination: {
+    //         el: '.main__slider .swiper-pagination',
+    //         type: 'bullets',
+    //         clickable: true,
+    //     },
+    //     breakpoints: {
+    //         1199: {
+    //             spaceBetween: 24,
+    //             slidesPerView: 4,
+    //         },
+    //         989: {
+    //             spaceBetween: 24,
+    //             slidesPerView: 3,
+    //         },
+    //         767: {
+    //             spaceBetween: 24,
+    //             slidesPerView: 2,
+    //         },
+    //         575: {
+    //             spaceBetween: 24,
+    //             slidesPerView: 1,
+    //         }
+    //     },
+    // });
+
+
+    // $('.footer__up').click(function () {
+    //     $("html, body").stop().animate({scrollTop: 0}, 800);
+    // });
+
+
+    // $('.burger').click(function () {
+    //     $('body').toggleClass('modal-open');
+    //     $('.header__mobile-open').toggleClass('active');
+    // });
+
+
+    // $('.page table').wrap("<div class='scrollbar scrollbar--horizontal'></div>");
+    // document.querySelectorAll('.scrollbar').forEach(el => {
+    //     new SimpleBar(el, {
+    //         autoHide: false
+    //     });
+    // });
+
+
+    
+
+
+
+
+
+});
